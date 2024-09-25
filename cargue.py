8f4bfd7c56d06f788ccd0dc813a9d08b71f5a2fe
Pinguill <pinguill@javerianacali.edu.co>
2024-09-25 17:02:46 -0500

    Creacion rapida tabla

--- cargue.py
+++ cargue.py
@@ -1,6 +1,32 @@
 import os
+import pandas as pd
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
+from sqlalchemy import create_engine
+
+# Cargue rápido de la tabla a PostgreSQL
+POSTGRES_CONN_STR = 'postgresql://postgres:admin@localhost:5432/rcdatos'
+
+def insertTable(name):
+    df = pd.read_csv('../../../../Lake/data/' + name)
+
+    # try:
+    #     df = pd.read_csv('../../../../Lake/data/' + name, encoding='utf-8', on_bad_lines='warn')
+    # except UnicodeDecodeError:
+    #     # Si falla, intentar con latin1
+    #     print(f"Error de codificación con utf-8 en {name}, intentando con latin1.")
+    #     df = pd.read_csv('../../../../Lake/data/' + name, encoding='latin1', on_bad_lines='warn')
+
+
+    engine = create_engine(POSTGRES_CONN_STR)
+    
+    nombre_archivo = os.path.basename('../../../../Lake/data/' + name).replace('.CSV', '')
+    
+    if len(nombre_archivo) > 32:
+        nombre_archivo = nombre_archivo[0:55].replace('_', '')
+
+    # Cargar datos en PostgreSQL, creando la tabla si no existe
+    df.to_sql(nombre_archivo, engine, if_exists='replace', index=False, schema='public')
 
 # Configura el blueprint para el módulo de cargue
 cargue_bp = Blueprint('cargue', __name__)
@@ -14,6 +40,8 @@ if not os.path.exists(UPLOAD_FOLDER):
 def cargue():
     if request.method == 'POST':
         new_filename = request.form.get('new_filename')
+        quick_create = 'quick_create' in request.form
+
         if 'file' not in request.files:
             
             flash('No se ha seleccionado ningún archivo')
@@ -34,6 +62,16 @@ def cargue():
             else:
                 filename = original_filename
 
+            if quick_create:
+                flash(f'Archivo "{filename}" subido y se creará la tabla rápidamente.')
+
+                # Llamado de la funcion para insertar la tabla
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
+                insertTable(filename)
+                file.save(os.path.join('../../../../Lake/procesados', filename))
+                flash(f'Tabla {filename} creada exitosamente')
+                return redirect(url_for('cargue.cargue'))
+            
             file.save(os.path.join(UPLOAD_FOLDER, filename))
             flash(f'Archivo {filename} cargado exitosamente')
             return redirect(url_for('cargue.cargue'))
